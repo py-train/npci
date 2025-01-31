fca4a81a929d0969ebbaaa313f80fde78f5a548a
Ashish Gulati <ashishgulati@Ashishs-MacBook-Air.local>
2025-01-31 10:55:07 +0530

    Latest state at Friday morn

--- morn/firstsite/firstsite/settings.py
+++ morn/firstsite/firstsite/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'import_export',
     "myapp",
 ]
 
@@ -44,7 +45,7 @@ MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
-    "django.middleware.csrf.CsrfViewMiddleware",
+    #"django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
@@ -75,10 +76,18 @@ WSGI_APPLICATION = "firstsite.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
+    "local": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
-    }
+    },
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "djangodb",
+        "USER": "admin",
+        "PASSWORD": "djangotestserver",
+        "HOST": "demo.chscr40jxrsi.ap-south-1.rds.amazonaws.com",
+        "PORT": 3306
+    },
 }
 
 
@@ -117,6 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
